2f5f3d7d179afecbf4eb820620196b58f3d186ef
ybaek9001 <ybaek9001@gmail.com>
2015-11-18 14:58:25 +0900

    선필 vo

---
+++ src/com/sportsforall/webprj/vo/Instant_Message.java
@@ -0,0 +1,13 @@
+package com.sportsforall.webprj.vo;
+
+import java.sql.Date;
+
+public class Instant_Message {
+	String code;
+	String member_email;
+	String title;
+	String contents;
+	Date regdate;
+	int hits;
+	
+}
